41f66e25c3a84463a6e0f003353f85487c52e1e3
asoukoulis <asoukoulis@mcclatchyservices.com>
2019-08-07 06:11:34 +0300

    config updates

--- scripts/host-setup.sh
+++ scripts/host-setup.sh
@@ -2,6 +2,7 @@
 
 OPENRESTY_VERSION="1.15.8.1"
 
+echo "Downloading openresty, installing dependencies, installing openresty..."
 wget "https://openresty.org/download/openresty-${OPENRESTY_VERSION}.tar.gz" && \
     tar -xvf "openresty-${OPENRESTY_VERSION}.tar.gz" && \
     cd "openresty-${OPENRESTY_VERSION}" && \
@@ -10,4 +11,10 @@ wget "https://openresty.org/download/openresty-${OPENRESTY_VERSION}.tar.gz" && \
     sudo apt-get install libreadline-dev libncurses5-dev libpcre3-dev libssl-dev perl && \
     ./configure -j2 --with-pcre-jit --with-ipv6 && \
     make -j2 && \
-    sudo make install
+    sudo make install && \
+    echo "OpenResty installation complete" && \
+    sleep 2 && \
+    echo "===============================" && \
+    echo "===============================" && \
+    echo "Adding openresty service to systemd..." && \
+    sudo cp system-config/openresty.service /etc/systemd/system/openresty.service
