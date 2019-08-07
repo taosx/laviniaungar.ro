1c262637ff4308a1b151b60cd536dd3bc12590d0
asoukoulis <asoukoulis@mcclatchyservices.com>
2019-08-07 06:54:37 +0300

    under construction resources

--- scripts/host-setup.sh
+++ scripts/host-setup.sh
@@ -7,9 +7,10 @@ wget "https://openresty.org/download/openresty-${OPENRESTY_VERSION}.tar.gz" && \
     tar -xvf "openresty-${OPENRESTY_VERSION}.tar.gz" && \
     cd "openresty-${OPENRESTY_VERSION}" && \
     sudo apt update && \
-    sudo apt-get install build-essential && \
-    sudo apt-get install libreadline-dev libncurses5-dev libpcre3-dev libssl-dev perl && \
-    ./configure -j2 --with-pcre-jit --with-ipv6 && \
+    sudo apt-get install build-essential -qq && \
+    sudo apt-get install libreadline-dev libncurses5-dev zlib1g zlib1g-dev \
+        libpcre3 libpcre3-dev libssl-dev perl -qq && \
+    ./configure -j2 --with-pcre-jit --with-ipv6 --enable-silent-rules && \
     make -j2 && \
     sudo make install && \
     echo "OpenResty installation complete" && \
@@ -17,4 +18,23 @@ wget "https://openresty.org/download/openresty-${OPENRESTY_VERSION}.tar.gz" && \
     echo "===============================" && \
     echo "===============================" && \
     echo "Adding openresty service to systemd..." && \
-    sudo cp system-config/openresty.service /etc/systemd/system/openresty.service
+    sudo cp -f system-config/openresty.service /etc/systemd/system/openresty.service && \
+    echo "Setting openresty configuration..." && \
+    sudo mkdir /usr/local/openresty/nginx/sites  && \
+    sudo mkdir /var/log/openresty && \
+    sudo cp -rf openresty-config/ conf/nginx.conf && \
+    sudo rm -f /usr/local/openresty/nginx/html/index.html && \
+    sudo mkdir /usr/local/openresty/nginx/html/default && \
+    sudo cp -f /usr/local/openresty/nginx/html/index.html /usr/local/openresty/nginx/html/default && \
+    echo "Enabling OpenResty service to start on boot..." && \
+    sudo systemctl daemon-reload && \
+    sudo systemctl restart openresty && \
+    echo "Starting open resty..."
+
+
+
+
+    
+    
+
+
